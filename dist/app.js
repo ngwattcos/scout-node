e67f6c80edcb4124e2f3505a875ac50c671bf62b
ngwattcos <curiosity142000@gmail.com>
2019-09-21 19:01:49 -0400

    port fix

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-var port = 3000;
-app.get('/', function (req, res) {
-    res.send('The sedulous hyena ate the antelope!');
-});
+var port = process.env.PORT || 3000;
 app.listen(port, function (err) {
     if (err) {
         return console.error(err);
--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,18 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, err => {
-	if (err) {
-		return console.error(err);
-	}
+// app.listen(port, (err: any) => {
+// 	if (err) {
+// 		return console.error(err);
+// 	}
 
-	return console.log(`server is listening on ${port}`);
-});
+// 	return console.log(`server is listening on ${port}`);
+// });
 
 app.get("/", (req, res) => {
 	console.log("main page");
 	res.send({
-		msg: "you have gotted this"
+		msg: `you have gotted this on port ${port}`
 	});
 })
 
